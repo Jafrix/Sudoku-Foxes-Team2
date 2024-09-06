ef21c20e0b550cfb1a623471392b62c3c594828a
Nikita <felasso3212@gmail.com>
2024-09-06 12:05:07 +0300

    func read release 1

--- sudoku.js
+++ sudoku.js
@@ -1,3 +1,21 @@
+const cfonts = require('cfonts');
+
+cfonts.say('Hello|world!', {
+	font: 'block',              // define the font face
+	align: 'left',              // define text alignment
+	colors: ['system'],         // define all colors
+	background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+	letterSpacing: 1,           // define letter spacing
+	lineHeight: 1,              // define the line height
+	space: true,                // define if the output text should have empty lines on top and on the bottom
+	maxLength: '0',             // define how many character can be on one line
+	gradient: false,            // define your two gradient colors
+	independentGradient: false, // define if you want to recalculate the gradient for each new line
+	transitionGradient: false,  // define if this is a transition between colors directly
+	rawMode: false,             // define if the line breaks should be CRLF (`\r\n`) over the default LF (`\n`)
+	env: 'node'                 // define the environment cfonts is being executed in
+});
+
 const fs = require('fs');
 const { EOL } = require('os');
 
@@ -11,13 +29,22 @@ function read() {
 }
 
 read()
+const arr = [
+  [1, 2, '-', 4, 5, 6, 7, 8, 9]
+  [9, 1, 2, 3, '-', 5, 6, 7, 8]
+  [1, 2, '-', 4, 5, 6, 7, 8, 9]
+  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  [1, 2, 3, 4, '-', 6, 7, 8, 9]
+  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  [1, 2, '-', 4, 5, 6, 7, 8, 9]
+  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+]
+function solve(arr) {
+// const arrOfRead = read();
 
-function solve() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции read.
-   * Возвращает игровое поле после попытки его решить.
-   */
 }
+solve(arr)
 
 function isSolved() {
   /**
